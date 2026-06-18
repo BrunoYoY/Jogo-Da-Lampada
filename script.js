24d90fb00ce9d54ab8168cd3f7ff9b91478a8564
BrunoYoY <brunowalterarantes@gmail.com>
2026-06-17 21:59:05 -0400

    Criando o sistema da lampada acender e apagar e quebrar

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 console.log("Loading serve data ...");
 
 window.alert("Bem Vindo ao Lamp Game. \nPara Jogar pressione 'OK");
+
+var acesa = false
+var quebrado = false
+
+function acenderlampada(){
+    document.getElementById("image").src = "img/lampada-acesa.jpg";
+}
+
+
+function quebrarlampada(){
+    if (quebrado == false){
+    quebrado = true;
+     document.getElementById("image").src = "img/lampada-quebrada.jpg";
+    }
+}
+
+function apagarlampada (){
+document.getElementById("image").src = "img/lampada-apagada.jpg";
+
+}
+function interruptor(){
+    if (quebrado == true){
+    return;
+}
+    if (acesa == false){
+           acenderlampada();
+ }
+ else{ apagarlampada();
+
+ }
+ acesa = !acesa;
+}
